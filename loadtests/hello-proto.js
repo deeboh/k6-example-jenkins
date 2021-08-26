50299f9c7cc5dfca79542aa25000f81a3b0217df
deeboh <micke.ficke@gmail.com>
2021-08-26 16:38:10 -0700

    Create hello-proto.js
    testing protobuf

---
+++ loadtests/hello-proto.js
@@ -0,0 +1,22 @@
+import grpc from 'k6/net/grpc';
+
+const client = new grpc.Client();
+client.load(['definitions'], 'hello.proto');
+
+export default () => {
+  client.connect('grpcbin.test.k6.io:9001', {
+    // plaintext: false
+  });
+
+  const data = { greeting: 'Bert' };
+  const response = client.invoke('hello.HelloService/SayHello', data);
+
+  check(response, {
+    'status is OK': (r) => r && r.status === grpc.StatusOK,
+  });
+
+  console.log(JSON.stringify(response.message));
+
+  client.close();
+  sleep(1);
+};
